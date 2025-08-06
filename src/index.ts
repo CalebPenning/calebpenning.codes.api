3ad534eb6120e43557dae28ef91842bf7ac38e41
Caleb Penning <calebpenning@mail.com>
2025-08-05 20:57:00 -0700

    add health check for deployment

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ const app = express()
 app.use(express.json())
 app.use(morgan("tiny"))
 
+app.get("/health", (_, res) => {
+	return res.status(200).send("OK")
+})
+
 app.use("/api/weather", weatherRouter)
 
 app.listen(process.env.PORT || 3000)
